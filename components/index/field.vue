660eeaad6bba5ae74cc923202ad15d11a933c8f1
clm <limin_chu@163.com>
2020-09-15 18:04:38 +0800

    test

--- components/index/field.vue
+++ components/index/field.vue
@@ -1,61 +1,71 @@
 <template>
   <el-row type="flex" justify="center" :gutter="16">
     <el-col>
-      <el-card shadow="hover">
+      <el-card shadow="hover" :body-style="{ padding: '0px' }">
         <el-image src="/img/field-01.jpg">
-          <div slot="error" class="image-slot">
+          <div
+            slot="error"
+            class="image-slot"
+            @mouseover.native="handlMouseOver"
+            @mouseleave.native="handlMouseLeave"
+          >
             <i class="el-icon-picture-outline"></i>
           </div>
         </el-image>
-        <div>
-          11111
+        <div :style="active" class="text-desc">
+          仓储物流
         </div>
       </el-card>
     </el-col>
     <el-col>
-      <el-card shadow="hover">
+      <el-card
+        shadow="hover"
+        :body-style="{ padding: '0px' }"
+        @mouseover.native.prevent="handlMouseOver"
+        @mouseleave.native.prevent="handlMouseLeave"
+      >
         <el-image src="/img/field-02.jpg">
           <div slot="error" class="image-slot">
             <i class="el-icon-picture-outline"></i>
           </div>
         </el-image>
-        <div>
+        <div class="text-desc">
           11111
         </div>
       </el-card>
     </el-col>
     <el-col>
-      <el-card shadow="hover">
+      <el-card shadow="hover" :body-style="{ padding: '0px' }">
         <el-image src="/img/field-03.jpg">
           <div slot="error" class="image-slot">
             <i class="el-icon-picture-outline"></i>
           </div>
         </el-image>
-        <div>
+        <div class="text-desc">
           11111
         </div>
       </el-card>
     </el-col>
     <el-col>
-      <el-card shadow="hover">
+      <el-card shadow="hover" :body-style="{ padding: '0px' }">
         <el-image src="/img/field-04.jpg">
           <div slot="error" class="image-slot">
             <i class="el-icon-picture-outline"></i>
           </div>
         </el-image>
-        <div>
+        <div class="text-desc">
           11111
         </div>
       </el-card>
     </el-col>
     <el-col>
-      <el-card shadow="hover">
-        <el-image src="/img/field-05.jpg">
+      <el-card shadow="hover" :body-style="{ padding: '0px' }">
+        <el-image src="/img/-05.jpg">
           <div slot="error" class="image-slot">
             <i class="el-icon-picture-outline"></i>
           </div>
         </el-image>
-        <div>
+        <div class="text-desc">
           11111
         </div>
       </el-card>
@@ -68,9 +78,37 @@ export default {
   name: 'Field',
   components: {},
   data() {
-    return {}
+    return {
+      active: ''
+    }
+  },
+  methods: {
+    handlMouseOver() {
+      this.active = 'background-color:#409EFF;'
+    },
+    handlMouseLeave() {
+      this.active = ''
+    }
   }
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.el-row
+  border 1px solid red
+  .el-card
+    >>>
+      .el-image
+        width 100%
+        min-height 202px
+        text-align center
+        color $color-PlaceholderText
+        .image-slot
+          i
+            font-size 100px
+      .text-desc
+        font-size 18px
+        font-weight 600
+        text-align center
+        border 1px solid red
+</style>
